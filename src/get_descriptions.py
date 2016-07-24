1d1045b9e6e184507237155d184ada12d75dc98c
Lucie Klimosova <lucka.molkova@gmail.com>
2016-07-24 11:02:24 -0700

    .ipynb and mongodb > .py and postgres

---
+++ src/get_descriptions.py
@@ -0,0 +1,110 @@
+from __future__ import division
+import requests
+import collections
+import json
+import psycopg2
+import time
+import datetime
+from multiprocessing.pool import ThreadPool
+from functools import partial
+
+
+requests.adapters.DEFAULT_RETRIES = 2
+
+
+def get_next_n(conn, limit=0):
+    ''' Gets description (first comment) for n bugs and stores it in PostgreSQL database.
+
+    Args:
+        conn: Psycopg2 connection to PostgreSQL database.
+        limit (int): Number of bugs to get description for.
+
+    Returns:
+        None.
+    '''
+    cur = conn.cursor()
+    cur.execute("SET TIME ZONE 'UTC';")
+
+    query = """
+            SELECT r.id
+            FROM reports r
+                LEFT OUTER JOIN descriptions d
+                    ON r.id = d.id
+            WHERE d.id IS NULL
+                -- exclude bugs that are not publicly available
+                AND r.id NOT IN (516716, 802791, 808848, 808853,
+                                821596, 627634, 525063, 661158,
+                                758675, 773487, 808808, 804674,
+                                724355, 795589)
+            LIMIT {}
+            """.format(limit)
+    cur_report = conn.cursor()
+    cur_report.execute(query)
+
+    # use threads to parallelize API requests
+    pool = ThreadPool(10)
+    bugs = cur_report.fetchall()
+    get_bug_desc_conn = partial(get_bug_desc, conn)
+    pool.map(get_bug_desc_conn, bugs)
+    pool.close()
+    return
+
+
+def get_bug_desc(conn, bug):
+    ''' Uses bugzilla API to get first bug comment and stores it in PostgreSQL database.
+
+    Args:
+        conn: Psycopg2 connection to PostgreSQL database.
+        bug (array): Array, the first item is bug_id (integer).
+
+    Returns:
+        None. Fetched data is inserted into descriptions table.
+    '''
+    cur = conn.cursor()
+
+    bug_id = str(bug[0])
+    # format to https://bugzilla.mozilla.org/rest/bug/707428/comment
+    website = 'https://bugzilla.mozilla.org/rest/bug/{}/comment'.format(bug_id)
+
+    content = requests.get(website)
+
+    # try to parse json data
+    try:
+        json_content = json.JSONDecoder().decode(content.content)
+        bug_date = json_content['bugs'][bug_id]['comments'][0]['creation_time']
+        bug_desc = json_content['bugs'][bug_id]['comments'][0]['raw_text']
+    except:
+        print "Invalid format for bug ", bug_id, ": ", content
+        return
+
+    # insert into database
+    cur.execute("""
+        INSERT INTO descriptions
+        VALUES (%s, to_timestamp(%s, 'YYYY-MM-DD HH24:MI:SS') - interval '9 h', %s)
+        """,
+        [bug_id, bug_date, bug_desc]
+    )
+    return
+
+if __name__ == "__main__":
+    try:
+        conn = psycopg2.connect("dbname='bugs' user='lucka' host='localhost'")
+    except:
+        print "Unable to connect to the database"
+
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS descriptions (
+        id            bigint NOT NULL,
+        when_created  timestamp,
+        descr         text
+        );
+    """)
+    conn.commit()
+
+    for i in xrange(1000):
+        get_next_n(conn, limit=100)
+        print "{} inserted 100 rows".format(datetime.datetime.now())
+        conn.commit()
+
+    conn.close()
---
+++ src/get_json_data.py
@@ -0,0 +1,70 @@
+from __future__ import division
+import psycopg2
+import collections
+import json
+import datetime
+from create_tables import create_all_tables
+
+
+def load_table(conn, json_file):
+    '''Moves data from json file to PostgreSQL table with the same name.
+
+    Args:
+        conn: Psycopg2 connection to PostgreSQL database.
+        json_file (str):  Name of json file to be loaded (without extension).
+
+    Returns:
+        None.
+    '''
+    cur = conn.cursor()
+    cur.execute("SET TIME ZONE 'UTC';")
+
+    data_path = '/Users/lucka/galvanize/msr2013-bug_dataset/data/v02/mozilla/'
+    json_path = data_path + json_file + '.json'
+    print '{}: processing {}...'.format(datetime.datetime.now(), json_path)
+
+    with open(json_path, 'r') as f:
+        data = json.JSONDecoder(
+            object_pairs_hook=collections.OrderedDict).decode(f.read())
+        for key, val in data[json_file].iteritems():
+            # reports
+            if type(val) == collections.OrderedDict:
+                query = 'INSERT INTO reports VALUES (to_timestamp(%s), %s, %s, %s, %s)'
+                cur.execute(query, val.values() + [int(key)])
+            # other tables
+            else:
+                query = 'INSERT INTO {} VALUES (to_timestamp(%s), %s, %s, %s)'.format(
+                    json_file)
+                for x in val:
+                    cur.execute(query, x.values() + [int(key)])
+    conn.commit()
+    return
+
+if __name__ == "__main__":
+
+    try:
+        conn = psycopg2.connect("dbname='bugs' user='lucka' host='localhost'")
+    except:
+        print "Unable to connect to the database"
+        exit(1)
+
+    tables = [
+        'reports',
+        'assigned_to',
+        'bug_status',
+        'cc',
+        'component',
+        'op_sys',
+        'priority',
+        'product',
+        'resolution',
+        'severity',
+        'short_desc',
+        'version',
+    ]
+    create_all_tables(conn, tables)
+
+    for table in tables:
+        load_table(conn, table)
+
+    conn.close()
