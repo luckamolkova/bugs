1b403b8d59a4c91b189bfd8a9e596a57fc42190a
Lucie Klimosova <lucka.molkova@gmail.com>
2016-08-09 11:03:58 -0700

    lowercase bug form inputs before processing

--- src/static/js/process_bug.js
+++ src/static/js/process_bug.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $("form").on('submit', function() {
         $.ajax({url: "/model",
-                data: {'product': $("input[name='product']").val(),
-                        'component': $("input[name='component']").val(),
-                        'assignee': $("input[name='assignee']").val(),
-                        'cc': $("input[name='cc']").val(),
-                        'short_desc': $("input[name='short_desc']").val(),
-                        'op_sys': $("input[name='op_sys']").val(),
-                        'desc': $("textarea[name='desc']").val()
+                data: {'product': $("input[name='product']").val().toLowerCase(),
+                        'component': $("input[name='component']").val().toLowerCase(),
+                        'assignee': $("input[name='assignee']").val().toLowerCase(),
+                        'cc': $("input[name='cc']").val().toLowerCase(),
+                        'short_desc': $("input[name='short_desc']").val().toLowerCase(),
+                        'op_sys': $("input[name='op_sys']").val().toLowerCase(),
+                        'desc': $("textarea[name='desc']").val().toLowerCase()
                 },
                 async: false,
                 success: function(result) {
